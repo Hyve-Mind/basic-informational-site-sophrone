539a447e34eab07ac717d5ef1b31cf5b8349cb87
sophrone <46854009+sophrone@users.noreply.github.com>
2023-05-23 09:12:42 +0100

    Final commit

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 const http = require("http");
 const fs = require("fs");
 
+const port = 8080;
+
 const server = http.createServer((req, res) => {
-  const url = req.url;
-  const fileName = url === "/" ? "./index.html" : `.${url}.html`;
-  fs.readFile(fileName, "utf-8", (err, data) => {
-    if (err) {
-      return fs.readFile("./404.html", "utf-8", (err, errorData) => {
-        if (err) {
-          throw err;
-        }
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(errorData);
-        console.log("new-error", errorData);
-        return res.end();
-      });
-    }
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(data);
-    return res.end();
-  });
+  if (req.url === "/") {
+    fs.readFile("index.html", function (err, data) {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end;
+    });
+  } else if (req.url === "/about") {
+    fs.readFile("about.html", function (err, data) {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end;
+    });
+  } else if (req.url === "/contact-me") {
+    fs.readFile("contact-me.html", function (err, data) {
+      if (err) throw err;
+      res.writeHead(200, { "Contact-Type": "text/html" });
+      res.write(data);
+      return res.end;
+    });
+  } else {
+    fs.readFile("404.html", function (err, data) {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end;
+    });
+  }
 });
 
-console.log("server created");
-
-server.listen(8080);
-
-console.log("server started, listening on port 8080");
+server.listen(port, () => {
+  console.log(`Server running at port: ${port}`);
+});
